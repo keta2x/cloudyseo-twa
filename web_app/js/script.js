2ed0a7dfbfb8a6ed6117c52d8d5624c4370cf30c
keta2x <158318631+keta2x@users.noreply.github.com>
2025-08-09 23:39:42 +0300

    Update script.js

--- web_app/js/script.js
+++ web_app/js/script.js
@@ -802,7 +802,7 @@ const PRIZE_NOTIFICATION_CONFIG = {
     prizeNameTop: '46%',
     prizeNameOffsetX: '14%',
     buttonBottom: '-15%',
-    prizeNameBottom: '20%',
+    prizeNameBottom: '25%',
 };
 
 let prizeNotificationOverlay = null;
@@ -2779,5 +2779,6 @@ async function main() {
 document.documentElement.style.setProperty('--ring-scale', WHEEL_GEOMETRY_CONFIG.outerRingScale);
 
 main();
+
 
 
